fe7632cf7a4d410dabf748c978dc9eaed61fdc29
lluc <lluccardoner@gmail.com>
2017-03-06 13:36:35 +0100

    prova

--- features/OneClassSVM_CNN_fc7.py
+++ features/OneClassSVM_CNN_fc7.py
@@ -5,45 +5,43 @@ import load_and_set as l
 
 np.set_printoptions(threshold=np.nan)
 
-# x = np.array([[-1, -1], [-2, -1], [1, 1], [2, 1]])
-# y = np.array([1, 1, 2, 2])
-
 total_video_num_devtest = 52
 total_video_num_testset = 26
 
-num_features_color_hist = 128
+
+num_features_fc7 = 4096
 clf = SVC(probability=True)
 
-x = np.empty((0, num_features_color_hist))
+x = np.empty((0, num_features_fc7))
 y = np.empty((0, 1), dtype=int)
 
-p = np.empty((0, num_features_color_hist))
+p = np.empty((0, num_features_fc7))
 q = np.empty((0, 1))
 
+print 'Start training'
 # get the devset data
 for i in range(total_video_num_devtest):
-    x = np.append(x, l.get_color_hist(i, 'devset'), axis=0)
+    x = np.append(x, l.get_fc7(i, 'devset'), axis=0)
     y = np.append(y, l.get_annotations('image', i, 'devset'))
 
-print y
 # train the classifier
-print 'Start training'
 t0 = time.time()
 clf.fit(x, y)
 print("Training time: %s s" % (time.time() - t0))
 
 # get the set data and write to file
-open('/home/lluc/Documents/ME16IN/devset/scripts/me16in_wien_image_colorhist.txt', 'w').close()
-f = open('/home/lluc/Documents/ME16IN/devset/scripts/me16in_wien_image_colorhist.txt', 'a')
+open('/home/lluc/Documents/ME16IN/devset/scripts/me16in_wien_image_fc7.txt', 'w').close()
+f = open('/home/lluc/Documents/ME16IN/devset/scripts/me16in_wien_image_fc7.txt', 'a')
 
 for i in range(total_video_num_devtest, total_video_num_devtest + total_video_num_testset):
     # p = np.append(p, lf.get_color_hist(i, 'testset'), axis=0)
     # q = np.append(q, lf.get_annotations('image', i, 'testset'))
-    p = l.get_color_hist(i, 'testset')
+    p = l.get_fc7(i, 'testset')
     q = l.get_annotations('image', i, 'testset')
 
     # test the classifier
     res = clf.predict(p)
     prob = clf.predict_proba(p)
+
     # write results to file
-    l.set_results(i, res, prob, f, 'ColorHist')
+    l.set_results(i, res, prob, f, 'fc7')
